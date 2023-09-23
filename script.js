9d1b8024ceb1b8424eb066a1a9016891c60b211e
BiwashGurung <varungrg10@gmail.com>
2023-09-23 18:56:29 +0545

    t

--- script.js
+++ script.js
@@ -67,5 +67,30 @@ backProfileBtn.onclick = () => {
 }
 
 //opening animaion
-const coverRight = document.querySelector('cover.coverright');
+const coverRight = document.querySelector('cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
+
+//opening animation (cover right animation) 
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100)
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+
+//opening animation (all page right animation)
+ pages.forEach((_, index) => {
+    setTimeout(() => { 
+        reverseIndex();
+        pages [pageNumber].classList.remove('turn');
+
+        setTimeout(() => {
+            reverseIndex();
+            pages [pageNumber].style.zIndex = 10 + index;
+            }, 500)
+
+    }, (index + 1) * 200 + 2100)
+})
 
